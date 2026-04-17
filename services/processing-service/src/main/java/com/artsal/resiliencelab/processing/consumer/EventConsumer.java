ed84cab38af816f4b4baaecde23f390b338db49a
Arthur Salla <arthurs81@gmail.com>
2026-04-17 15:35:16 +0530

    feat: enhance event consumer with simulated failure handling and improved logging

--- services/processing-service/src/main/java/com/artsal/resiliencelab/processing/consumer/EventConsumer.java
+++ services/processing-service/src/main/java/com/artsal/resiliencelab/processing/consumer/EventConsumer.java
@@ -4,11 +4,24 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class EventConsumer {
+    private final Random random = new Random();
+
     @KafkaListener(topics = "events-topic", groupId = "resilience-group")
     public void consumeEvent(Map<String, Object> event) {
-        System.out.println("🔥 Received event: " + event);
+
+        System.out.println("📥 Received event: " + event);
+
+        // 🔥 Random failure (50% chance)
+        if (random.nextBoolean()) {
+            System.out.println("💥 Simulated failure for event: " + event.get("eventId"));
+            throw new RuntimeException("Simulated failure");
+        }
+
+        System.out.println("✅ Successfully processed event: " + event.get("eventId"));
     }
+
 }
